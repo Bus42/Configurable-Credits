25f8ede4e576d650f612b562610c5f1c52500aee
Bus42 <greg@studio42dev.com>
2025-03-27 13:29:59 -0600

    feat: add image and text configuration for intro and outro displays

--- src/js/config.js
+++ src/js/config.js
@@ -13,3 +13,29 @@ const socketConfig = {
 const environmentConfig = {
     testing: true
 };
+
+const imageConfig = {
+    introImage: {
+        src: 'https://res.cloudinary.com/studio42-web-development/image/upload/v1739058435/Gaming/kav9svkuerviqk7rmloi.png',
+        alt: 'Satan\'s Gerbils',
+        style: {
+            width: 800,
+            height: 600
+        },
+        show: true
+    },
+    outroImage: {
+        src: 'https://res.cloudinary.com/studio42-web-development/image/upload/v1671603099/Gaming/daer0mimlj7u0ep5ftoe.jpg',
+        alt: 'So long, and thanks for all the fish!',
+        style: {
+            width: 800,
+            height: 600,
+        },
+        show: true
+    }
+};
+
+const textConfig = {
+    introText: 'Thanks for watching!',
+    outroText: 'So long, and thanks for all the fish!'
+};
