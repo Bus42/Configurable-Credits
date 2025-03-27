382c91c59c1c9ec1c4ceb42555417939e83c549b
Bus42 <greg@studio42dev.com>
2025-03-27 13:33:45 -0600

    feat: add detailed configuration types for image and text settings

--- src/js/config.js
+++ src/js/config.js
@@ -9,11 +9,31 @@ const socketConfig = {
     port: 8080
 };
 
-
+/**
+ * Environment settings
+ * @type {EnvironmentConfig}
+ */
 const environmentConfig = {
     testing: true
 };
 
+/**
+ * @typedef {Object} ImageConfig
+ * @property {Object} introImage - Configuration for the intro image
+ * @property {string} introImage.src - Source URL of the intro image
+ * @property {string} introImage.alt - Alt text for the intro image
+ * @property {Object} introImage.style - Style properties for the intro image
+ * @property {number} introImage.style.width - Width of the intro image
+ * @property {number} introImage.style.height - Height of the intro image
+ * @property {boolean} introImage.show - Flag to show the intro image
+ * @property {Object} outroImage - Configuration for the outro image
+ * @property {string} outroImage.src - Source URL of the outro image
+ * @property {string} outroImage.alt - Alt text for the outro image
+ * @property {Object} outroImage.style - Style properties for the outro image
+ * @property {number} outroImage.style.width - Width of the outro image
+ * @property {number} outroImage.style.height - Height of the outro image
+ * @property {boolean} outroImage.show - Flag to show the outro image
+ */
 const imageConfig = {
     introImage: {
         src: 'https://res.cloudinary.com/studio42-web-development/image/upload/v1739058435/Gaming/kav9svkuerviqk7rmloi.png',
@@ -35,6 +55,11 @@ const imageConfig = {
     }
 };
 
+/**
+ * * @typedef {Object} TextConfig
+ * @property {string} introText - Introductory text to display
+ * @property {string} outroText - Outro text to display
+ */
 const textConfig = {
     introText: 'Thanks for watching!',
     outroText: 'So long, and thanks for all the fish!'
