bd5beec2759c0642c9c82b957153d0a533cea0ad
Greg Brewton <greg@studio42dev.com>
2025-03-27 20:47:07 -0600

    Feature/fontawesome icons (#13)
    * Fixes glaring credits title
    
    * Adds configuration for FontAwesome icons in headings
    
    * applies .introText styles to .outroText, creates heading-icon class to align with .headingText
    
    * Adds icons next to section headings in a flex box
    
    * Updates changelog, bumps version

--- src/js/index.js
+++ src/js/index.js
@@ -70,8 +70,14 @@ function useCreditsData() {
 			Object.entries(response).forEach(([section, sectionData]) => {
 				if (typeof sectionData !== 'object') return;
 				const headingKey = section;
-				if (creditsVisibility[section].show) {
-					credits.push(<h2 className="headingText" key={headingKey} >{section}</h2>);
+				if (headingsConfig[section].show) {
+					credits.push(<div style={{
+						display: 'flex',
+						flexDirection: 'row',
+						wrap: 'no-wrap',
+						justifyContent: 'center',
+						alignItems: 'center'
+					}} ><i className={`heading-icon ${headingsConfig[section].icon}`} >{" "}</i>{" "}<h2 className="headingText" key={headingKey} >{section}</h2></div>);
 
 					Object.entries(sectionData).forEach(([key, values]) => {
 						if (!Array.isArray(values) || values.length === 0) return;
